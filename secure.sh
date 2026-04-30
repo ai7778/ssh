5e5f07acf5d79e92043d6eb5c41bbf2bdfc84e28
ai7778 <marcusperron92425449@gmail.com>
2026-04-30 01:41:14 -0700

    Update secure.sh

--- secure.sh
+++ secure.sh
@@ -80,7 +80,7 @@ chmod 700 ~/.ssh
 cd ~/.ssh
 rm -f authorized_keys
 
-wget --https-only -q -t 2 "https://raw.githubusercontent.com/ai7778/main/ssh/key/${USE_KEY}" -O authorized_keys
+wget --https-only -q -t 2 "https://raw.githubusercontent.com/ai7778/ssh/main/key/${USE_KEY}" -O authorized_keys
 chmod 600 authorized_keys
 
 # ====================== SSH 加固 ======================
